9c63eb64298f45d2040f5af1a024f5da8a404e46
Lishadsza <22b18.lisha@sjec.ac.in>
2025-09-09 20:44:30 +0530

    changes in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss' // <-- Import from '@tailwindcss/postcss'
+import tailwindcss from '@tailwindcss/postcss' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", 
   css: {
     postcss: {
-      plugins: [tailwindcss()], // <-- This now correctly uses the imported plugin
+      plugins: [tailwindcss()],
     },
   },
 })
